f01e85a03dcc70ab632c6094d5701921a0ef62b5
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-07 17:33:34 -0500

    Created server listening on port 5000, and cretaed getRecipes to get list of all recipes

---
+++ recipes/recipes-router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const recipes = require('./recipes-model.js');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    recipes.getRecipes()
+    .then(recipes => {
+      res.json(recipes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipe' });
+    });
+  });
+
+module.exports = router;
