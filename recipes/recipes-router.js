80a6c411d546c471ed50e5dffde901a87c6d7aa5
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-08 10:11:27 -0500

    Mvp complete

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -16,13 +16,43 @@ router.get('/', (req, res) => {
   });
 
 
-  router.get('/', (req, res) => {
-    recipes.getRecipes()
-    .then(recipes => {
-      res.json(recipes);
+  router.get('/:id/shoppingList', (req, res) => {
+      const {id} =req.params;
+
+    recipes.getShoppingList(id)
+    .then(shoppingList => {
+      res.json(shoppingList);
     })
     .catch(err => {
       res.status(500).json({ message: 'Failed to get recipe' });
     });
   });
+
+  router.get('/:id/instructions', (req, res) => {
+    const {id} =req.params;
+
+  recipes.getInstructions(id)
+  .then(instructions => {
+    res.json(instructions);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipe' });
+  });
+});
+
+//   router.get('/:id/shoppingList', (req, res) => {
+//     const { id } = req.params;
+  
+//     recipes.getShoppingList(id)
+//     .then(steps => {
+//       if (steps.length) {
+//         res.json(steps);
+//       } else {
+//         res.status(404).json({ message: 'Could not find steps for given scheme' })
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to get steps' });
+//     });
+//   });
 module.exports = router;
